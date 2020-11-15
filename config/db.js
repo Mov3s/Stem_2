cb4110264a01306402aef04a0cd64c5668092a75
Isioma Chuck <isioma.chuck@gmail.com>
2020-11-15 21:53:30 +0000

    Added create counter if empty

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+const Counters = require('../models/Counters')
+
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
@@ -12,6 +14,26 @@ const connectDB = async () => {
     });
 
     console.log('MongoDB Connected...');
+
+    let count = await Counters.countDocuments({}).exec()
+
+    if (count === 0){
+      let entries = [
+        { _id: "reviewId", seq : 1}, 
+        { _id: "orderId", seq : 1}, 
+        { _id: "customerId", seq: 1}, 
+        { _id: "invoiceId", seq: 1}, 
+        { _id: "blogId", seq : 1},
+        { _id: "productId", seq : 1},
+        { _id: "userId", seq : 1},
+        { _id: "categoryId", seq : 1},
+        { _id: "extraId", seq : 1}
+      ]
+      
+      let idxs = await  Counters.insertMany(entries)
+    }
+    
+    
   } catch (err) {
     console.error(err.message);
     // Exit process with failure
